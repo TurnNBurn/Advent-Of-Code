8b9a8bc9abf452eb0bac014958354571c923d6bb
TurnNBurn <philieaglesfan88@gmail.com>
2021-12-01 23:20:53 -0600

    Add logic for Problem 1

--- Day 2/Day2.cs
+++ Day 2/Day2.cs
@@ -6,15 +6,35 @@ class AdventOfCodeDay2
     static void Main()
     {
         string[] lines = System.IO.File.ReadAllLines("./Problem1Input.txt");
-        int depthTimesHeight = Problem1(lines);
+        int depthTimesHoriz = Problem1(lines);
+
+        Console.WriteLine("The final depth times horizontal distance is " + depthTimesHoriz);
     }
 
     private static int Problem1(string[] lines)
     {
         int depth = 0;
-        int height = 0;
+        int horiz = 0;
+        foreach (string line in lines)
+        {
+            string[] words = line.Split(' ');
+            int distance = Convert.ToInt32(words[1]);
+            string direction = words[0].ToUpper();
+            if (direction.Equals("UP"))
+            {
+                depth -= distance;
+            }
+            if (direction.Equals("DOWN"))
+            {
+                depth += distance;
+            }
+            if (direction.Equals("FORWARD"))
+            {
+                horiz += distance;
+            }
+        }
 
-        return depth * height;
+        return depth * horiz;
     }
 
 }
