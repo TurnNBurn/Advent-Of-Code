8f33ef6c183d9eb9fa3b42c136f69696cd84dbaa
TurnNBurn <philieaglesfan88@gmail.com>
2021-12-01 23:13:46 -0600

    Add files for Day 2 problems

---
+++ Day 2/Day2.cs
@@ -0,0 +1,17 @@
+using System;
+
+class AdventOfCodeDay2
+{
+
+    static void Main()
+    {
+        string[] lines = System.IO.File.ReadAllLines("./Problem1Input.txt");
+
+    }
+
+    private static void Problem1(string[] lines)
+    {
+
+    }
+
+}
