54b4e8aecf7d533babc74c41f67152543ebd44e9
TurnNBurn <philieaglesfan88@gmail.com>
2021-12-01 23:15:48 -0600

    Update return structure

--- Day 2/Day2.cs
+++ Day 2/Day2.cs
@@ -6,12 +6,15 @@ class AdventOfCodeDay2
     static void Main()
     {
         string[] lines = System.IO.File.ReadAllLines("./Problem1Input.txt");
-
+        int depthTimesHeight = Problem1(lines);
     }
 
-    private static void Problem1(string[] lines)
+    private static int Problem1(string[] lines)
     {
+        int depth = 0;
+        int height = 0;
 
+        return depth * height;
     }
 
 }
