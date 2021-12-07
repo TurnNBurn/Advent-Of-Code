da4a5aeed91476deaf06141cb40107a23ca7761e
TurnNBurn <philieaglesfan88@gmail.com>
2021-12-07 12:17:43 -0600

    Parse bingo board input

--- Day 4/Day4.cs
+++ Day 4/Day4.cs
@@ -15,6 +15,61 @@ public class AdventOfCodeDay4
     private static int Problem1(string[] lines)
     {
         int bingoScore = 0;
+        string[] bingoNumbers = lines[0].Split(',');
         return bingoScore;
     }
+
+    private static List<Board> ParseBingoBoards(string[] lines)
+    {
+        List<Board> bingoBoards = new List<Board>();
+        List<string> boardContents = new List<string>();
+        for (int i = 2; i < lines.Length; i++)
+        {
+            if (lines[i].Equals(String.Empty))
+            {
+                Board temp = BuildBoard(boardContents);
+                bingoBoards.Add(temp);
+                boardContents.Clear();
+            }
+            else
+            {
+                boardContents.Add(lines[i]);
+            }
+        }
+        return bingoBoards;
+    }
+
+    private static Board BuildBoard(List<string> boardContents)
+    {
+        Board board = new Board();
+        List<List<int>> columns = new List<List<int>>();
+        for (int i = 0; i < boardContents.Count; i++)
+        {
+            string[] row = boardContents[i].Split(',');
+            List<int> rowList = new List<int>();
+            for (int j = 0; j < row.Length; j++)
+            {
+                int rowEntry = Convert.ToInt32(row[j]);
+                rowList.Add(rowEntry);
+                columns[j].Add(rowEntry);
+            }
+            board.solutions.Add(rowList);
+        }
+        foreach (List<int> column in columns)
+        {
+            board.solutions.Add(column);
+        }
+        return board;
+    }
+
+    public class Board
+    {
+        public Board()
+        {
+            solutions = new List<List<int>>();
+        }
+        public List<List<int>> solutions;
+
+    }
+
 }
