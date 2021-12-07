9cf4641460f1144d86c7148f7782b844feda04c7
TurnNBurn <philieaglesfan88@gmail.com>
2021-12-07 15:11:45 -0600

    Add file structure for Day 5

---
+++ Day 5/Day5.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class AdventOfCodeDay5
+{
+
+    public static void run()
+    {
+        string[] lines = System.IO.File.ReadAllLines("./Day 4/Problem1Input.txt");
+
+        Console.WriteLine("Day 5 - Problem 1: ");
+    }
+
+    private static void Problem1(string[] lines)
+    {
+
+    }
+
+}
