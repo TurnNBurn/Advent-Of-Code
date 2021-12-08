342b392f4bb7eae72b7bb367e6b07879817e8d2e
TurnNBurn <philieaglesfan88@gmail.com>
2021-12-07 21:51:26 -0500

    Implement problem 1

--- Day 6/Day6.cs
+++ Day 6/Day6.cs
@@ -6,12 +6,33 @@ public class AdventOfCodeDay6
     public static void run()
     {
         string[] lines = System.IO.File.ReadAllLines("./Day 6/Problem1Input.txt");
-        //int coordsWithMoreThanOneHit = Problem1(lines);
-        Console.WriteLine("Day 6 - Problem 1: ");
+        int numberOfFish = Problem1(lines);
+        Console.WriteLine("Day 6 - Problem 1: After 8 days there are " + numberOfFish + " fish.");
     }
 
-    private static void Problem1(string[] lines)
+    private static int Problem1(string[] lines)
     {
-
+        string[] initialFish = lines[0].Split(',');
+        List<int> fish = new List<int>();
+        foreach (string lanternFish in initialFish)
+        {
+            fish.Add(Convert.ToInt32(lanternFish));
+        }
+        for (int days = 0; days < 80; days++)
+        {
+            for (int i = 0; i < fish.Count; i++)
+            {
+                if (fish[i] == 0)
+                {
+                    fish[i] = 6;
+                    fish.Add(8);
+                }
+                else
+                {
+                    fish[i] = fish[i] - 1;
+                }
+            }
+        }
+        return fish.Count;
     }
 }
