28afe1ca0dc06e60eb908804de47ec64c7f4f387
TurnNBurn <philieaglesfan88@gmail.com>
2021-12-10 11:25:26 -0600

    Implement problem 2

--- Day 10/Day10.cs
+++ Day 10/Day10.cs
@@ -7,7 +7,9 @@ public class AdventOfCodeDay10
     {
         string[] lines = System.IO.File.ReadAllLines("./Day 10/Problem1Input.txt");
         int totalCorruptionScore = Problem1(lines);
+        long middleAutoCompleteScore = Problem2(lines);
         Console.WriteLine("Day 10 - Problem 1: The total corruption score is " + totalCorruptionScore);
+        Console.WriteLine("Day 10 - Problem 2: The middle autocomplete score is " + middleAutoCompleteScore);
     }
 
     private static int Problem1(string[] lines)
@@ -15,7 +17,7 @@ public class AdventOfCodeDay10
         int corruptedSum = 0;
         foreach (string line in lines)
         {
-            if (CheckForCorruption(line, out char corruptedChar))
+            if (CheckForCorruption(line, out char corruptedChar, out Stack<char> openChunk))
             {
                 corruptedSum += CorruptedCharValues[corruptedChar];
             }
@@ -23,33 +25,56 @@ public class AdventOfCodeDay10
         return corruptedSum;
     }
 
-    private static bool CheckForCorruption(string line, out char corruptedChar)
+    private static long Problem2(string[] lines)
+    {
+        List<long> autoCompleteScores = new List<long>();
+        foreach (string line in lines)
+        {
+            if (!CheckForCorruption(line, out char corruptedChar, out Stack<char> openChunk))
+            {
+                long lineScore = FindAutoCompleteScore(openChunk);
+                if (lineScore > 0)
+                {
+                    autoCompleteScores.Add(lineScore);
+                }
+            }
+        }
+        autoCompleteScores.Sort();
+        return autoCompleteScores[GetMiddleIndex(autoCompleteScores.Count - 1)];
+    }
+
+    private static int GetMiddleIndex(int count)
+    {
+        return (int)((count / 2.0) + 0.5);
+    }
+
+    private static bool CheckForCorruption(string line, out char corruptedChar, out Stack<char> openChunk)
     {
         corruptedChar = ' '; //Hacky and I don't like it
-        Stack<char> chunk = new Stack<char>();
+        openChunk = new Stack<char>();
         for (int i = 0; i < line.Length; i++)
         {
             switch (line[i])
             {
                 case '(':
-                    chunk.Push('(');
+                    openChunk.Push('(');
                     break;
                 case '[':
-                    chunk.Push('[');
+                    openChunk.Push('[');
                     break;
                 case '{':
-                    chunk.Push('{');
+                    openChunk.Push('{');
                     break;
                 case '<':
-                    chunk.Push('<');
+                    openChunk.Push('<');
                     break;
                 case ')':
                 case ']':
                 case '}':
                 case '>':
-                    if (chunk.Peek().Equals(CloseBracketPairs[line[i]]))
+                    if (openChunk.Peek().Equals(CloseBracketPairs[line[i]]))
                     {
-                        chunk.Pop();
+                        openChunk.Pop();
                     }
                     else
                     {
@@ -64,6 +89,17 @@ public class AdventOfCodeDay10
         return false;
     }
 
+    private static long FindAutoCompleteScore(Stack<char> openChunk)
+    {
+        long autoCompleteSum = 0;
+        while (openChunk.Count > 0)
+        {
+            autoCompleteSum *= 5;
+            autoCompleteSum += AutoCompleteCharVals[openChunk.Pop()];
+        }
+        return autoCompleteSum;
+    }
+
     private static Dictionary<char, char> CloseBracketPairs = new Dictionary<char, char>{
         {')', '('},
         {']', '['},
@@ -76,5 +112,12 @@ public class AdventOfCodeDay10
         {']', 57},
         {'}', 1197},
         {'>', 25137}
+    };
+
+    private static Dictionary<char, int> AutoCompleteCharVals = new Dictionary<char, int>{
+        {'(', 1},
+        {'[', 2},
+        {'{', 3},
+        {'<', 4}
     };
 }
