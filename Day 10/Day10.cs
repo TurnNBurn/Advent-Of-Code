aa9b4cba39a33841521e78b8a43e79387b829bb6
TurnNBurn <philieaglesfan88@gmail.com>
2021-12-10 10:51:11 -0600

    Implement problem 1

--- Day 10/Day10.cs
+++ Day 10/Day10.cs
@@ -12,6 +12,75 @@ public class AdventOfCodeDay10
 
     private static int Problem1(string[] lines)
     {
-        return 0;
+        List<char> corruptedChars = new List<char>();
+        foreach (string line in lines)
+        {
+            corruptedChars = CheckForCorruption(line, corruptedChars);
+        }
+        return SumCorruptedChars(corruptedChars);
     }
+
+    private static List<char> CheckForCorruption(string line, List<char> corruptedChars)
+    {
+        Stack<char> chunk = new Stack<char>();
+        for (int i = 0; i < line.Length; i++)
+        {
+            switch (line[i])
+            {
+                case '(':
+                    chunk.Push('(');
+                    break;
+                case '[':
+                    chunk.Push('[');
+                    break;
+                case '{':
+                    chunk.Push('{');
+                    break;
+                case '<':
+                    chunk.Push('<');
+                    break;
+                case ')':
+                case ']':
+                case '}':
+                case '>':
+                    if (chunk.Peek().Equals(CloseBracketPairs[line[i]]))
+                    {
+                        chunk.Pop();
+                    }
+                    else
+                    {
+                        corruptedChars.Add(line[i]);
+                        return corruptedChars;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return corruptedChars;
+    }
+
+    private static int SumCorruptedChars(List<char> corruptedChars)
+    {
+        int sum = 0;
+        foreach (char corrupted in corruptedChars)
+        {
+            sum += CorruptedCharValues[corrupted];
+        }
+        return sum;
+    }
+
+    private static Dictionary<char, char> CloseBracketPairs = new Dictionary<char, char>{
+        {')', '('},
+        {']', '['},
+        {'}', '{'},
+        {'>', '<'}
+    };
+
+    private static Dictionary<char, int> CorruptedCharValues = new Dictionary<char, int>{
+        {')', 3},
+        {']', 57},
+        {'}', 1197},
+        {'>', 25137}
+    };
 }
