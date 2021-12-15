e0919426f241f6f59c2c3dde3280e591cff35f72
TurnNBurn <philieaglesfan88@gmail.com>
2021-12-15 16:32:58 -0600

    Implement problem 2 with a priority queue. Leave problem 1 as is for posterity

--- Day 15/Day15.cs
+++ Day 15/Day15.cs
@@ -31,18 +31,9 @@ public class AdventOfCodeDay15
 
     private static int Problem2(string[] lines)
     {
-        int[,] caveMap = BuildCaveMap(lines);
+        int[,] caveMap = BuildBigCaveMap(lines);
         int[,] distanceMap = new int[caveMap.GetLength(0), caveMap.GetLength(1)];
-        if (caveMap[0, 1] <= caveMap[1, 0])
-        {
-            MoveOne(caveMap, distanceMap, 0, new Coordinates(0, 1));
-            MoveOne(caveMap, distanceMap, 0, new Coordinates(1, 0));
-        }
-        else
-        {
-            MoveOne(caveMap, distanceMap, 0, new Coordinates(1, 0));
-            MoveOne(caveMap, distanceMap, 0, new Coordinates(0, 1));
-        }
+        SearchCave(caveMap, distanceMap);
         return distanceMap[distanceMap.GetLength(0) - 1, distanceMap.GetLength(1) - 1];
     }
 
@@ -69,6 +60,10 @@ public class AdventOfCodeDay15
             return;
         }
 
+
+        //This is messy - but we have to check whether or not we can go in each direction
+        //And then which direction has the lowest risk. But we always have to traverse both directions
+        //The risk score just helps us know which to try first
         bool canGoRight = spot.x < caveLength;
         bool canGoDown = spot.y < caveWidth;
         //Search neighbors
@@ -100,6 +95,34 @@ public class AdventOfCodeDay15
         }
     }
 
+    private static void SearchCave(int[,] caveMap, int[,] distanceMap)
+    {
+        PriorityQueue<Coordinates, int> queue = new PriorityQueue<Coordinates, int>();
+        queue.Enqueue(new Coordinates(0, 0), 0);
+
+        while (queue.Count > 0)
+        {
+            Coordinates spot = queue.Dequeue();
+            foreach (Coordinates neighbor in spot.GetNeighbors(caveMap))
+            {
+                int distance = distanceMap[spot.x, spot.y] + caveMap[neighbor.x, neighbor.y];
+                if (distanceMap[neighbor.x, neighbor.y] != 0)
+                {
+                    if (distanceMap[neighbor.x, neighbor.y] > distance)
+                    {
+                        distanceMap[neighbor.x, neighbor.y] = distance;
+                        queue.Enqueue(neighbor, distance);
+                    }
+                }
+                else
+                {
+                    distanceMap[neighbor.x, neighbor.y] = distance;
+                    queue.Enqueue(neighbor, distance);
+                }
+            }
+        }
+    }
+
     private static int[,] BuildCaveMap(string[] lines)
     {
         int[,] caveMap = new int[lines[0].Length, lines.Length];
@@ -113,6 +136,31 @@ public class AdventOfCodeDay15
         }
         return caveMap;
     }
+
+    private static int[,] BuildBigCaveMap(string[] lines)
+    {
+        int[,] caveMap = new int[lines[0].Length * 5, lines.Length * 5];
+        for (int xCount = 0; xCount < 5; xCount++)
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                char[] line = lines[i].ToCharArray();
+                for (int yCount = 0; yCount < 5; yCount++)
+                {
+                    for (int j = 0; j < line.Length; j++)
+                    {
+                        int value = Convert.ToInt32(line[j].ToString()) + xCount + yCount;
+                        if (value > 9)
+                        {
+                            value = value % 9;
+                        }
+                        caveMap[(xCount * lines.Length) + i, (yCount * line.Length) + j] = value;
+                    }
+                }
+            }
+        }
+        return caveMap;
+    }
 }
 
 public class Coordinates
@@ -121,6 +169,29 @@ public class Coordinates
     {
         x = X;
         y = Y;
+    }
+
+    public List<Coordinates> GetNeighbors(int[,] caveMap)
+    {
+        List<Coordinates> neighbors = new List<Coordinates>();
+        if (x > 0)
+        {
+            neighbors.Add(new Coordinates(x - 1, y));
+        }
+        if (y > 0)
+        {
+            neighbors.Add(new Coordinates(x, y - 1));
+        }
+        if (x < caveMap.GetLength(0) - 1)
+        {
+            neighbors.Add(new Coordinates(x + 1, y));
+        }
+        if (y < caveMap.GetLength(1) - 1)
+        {
+            neighbors.Add(new Coordinates(x, y + 1));
+        }
+
+        return neighbors;
     }
 
     public override bool Equals(object? obj)
