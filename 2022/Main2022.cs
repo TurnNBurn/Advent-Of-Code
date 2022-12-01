79959242a4a3e0c0cd17145155075491d832b438
TurnNBurn <philieaglesfan88@gmail.com>
2022-12-01 19:07:09 +0100

    Add top level Main and 2022 specific Main

---
+++ 2022/Main2022.cs
@@ -0,0 +1,8 @@
+public class AdventOfCode2022
+{
+
+    public static void run()
+    {
+        AdventOfCode2022Day1.run();
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,8 @@
+class AdventOfCode
+{
+    static void Main()
+    {
+        AdventOfCode2021.run();
+        AdventOfCode2022.run();
+    }
+}
