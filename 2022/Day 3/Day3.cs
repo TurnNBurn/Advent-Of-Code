2b393e908b3b66ca30da9bef8880829d1487ea2a
TurnNBurn <philieaglesfan88@gmail.com>
2022-12-03 10:42:47 +0100

    Solve problem 2 for day 3

--- 2022/Day 3/Day3.cs
+++ 2022/Day 3/Day3.cs
@@ -8,10 +8,10 @@ public class AdventOfCode2022Day3
         string[] lines = System.IO.File.ReadAllLines("./2022/Day 3/Problem1Input.txt");
 
         int totalPriority = Problem1(lines);
-        //int totalOptimizedScore = Problem2(lines);
+        int totalBadgePriority = Problem2(lines);
 
         Console.WriteLine("Day 3 - Problem 1: The total priority of all the items split across compartments is " + totalPriority + ".");
-        //Console.WriteLine("Day 2 - Problem 2: My total score with optimized play is " + totalOptimizedScore + ".");
+        Console.WriteLine("Day 3 - Problem 2: The total priority of the badges is " + totalBadgePriority + ".");
     }
 
     private static int Problem1(string[] lines)
@@ -52,6 +52,43 @@ public class AdventOfCode2022Day3
 
     private static int Problem2(string[] lines)
     {
+        int sum = 0;
+        for (int i = 0; i < lines.Length - 2; i += 3)
+        {
+            sum += FindBadgePriority(lines[i], lines[i + 1], lines[i + 2]);
+        }
+        return sum;
+    }
+
+    private static int FindBadgePriority(string rucksackA, string rucksackB, string rucksackC)
+    {
+        HashSet<char> firstRucksackItems = new HashSet<char>();
+        foreach (char item in rucksackA)
+        {
+            firstRucksackItems.Add(item);
+        }
+        HashSet<char> sharedRucksackItems = new HashSet<char>();
+        foreach (char item in rucksackB)
+        {
+            if (firstRucksackItems.Contains(item))
+            {
+                sharedRucksackItems.Add(item);
+            }
+        }
+        if (sharedRucksackItems.Count == 1)
+        {
+            foreach (char item in sharedRucksackItems)
+            {
+                return GetPriorityForItem(item);
+            }
+        }
+        foreach (char item in rucksackC)
+        {
+            if (sharedRucksackItems.Contains(item))
+            {
+                return GetPriorityForItem(item);
+            }
+        }
         return -1;
     }
 }
