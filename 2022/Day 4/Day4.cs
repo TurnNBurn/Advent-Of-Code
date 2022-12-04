ded197422b00521b31089f3fff5085d8b72bc8ea
TurnNBurn <philieaglesfan88@gmail.com>
2022-12-04 09:35:06 +0100

    Add solutions

--- 2022/Day 4/Day4.cs
+++ 2022/Day 4/Day4.cs
@@ -7,19 +7,69 @@ public class AdventOfCode2022Day4
     {
         string[] lines = System.IO.File.ReadAllLines("./2022/Day 4/Problem1Input.txt");
 
-        int totalPriority = Problem1(lines);
-        //int totalBadgePriority = Problem2(lines);
+        int totalPairsFullyContained = Problem1(lines);
+        int totalPairsOverlapping = Problem2(lines);
 
-        Console.WriteLine("Day 4 - Problem 1: The total priority of all the items split across compartments is " + totalPriority + ".");
-        //Console.WriteLine("Day 4 - Problem 2: The total priority of the badges is " + totalBadgePriority + ".");
+        Console.WriteLine("Day 4 - Problem 1: There are " + totalPairsFullyContained + " elves with fully contained ranges.");
+        Console.WriteLine("Day 4 - Problem 2: There are " + totalPairsOverlapping + " pairs with overlapping segments.");
     }
 
     private static int Problem1(string[] lines)
     {
-        return -1;
+        int totalPairsFullyContained = 0;
+        foreach (string line in lines)
+        {
+            string[] pair = SplitPairs(line);
+            int[] firstRange = ParseOneRange(pair[0]);
+            int[] secondRange = ParseOneRange(pair[1]);
+            if (firstRange[0] <= secondRange[0] && firstRange[1] >= secondRange[1])
+            {
+                totalPairsFullyContained++;
+            }
+            else if (secondRange[0] <= firstRange[0] && secondRange[1] >= firstRange[1])
+            {
+                totalPairsFullyContained++;
+            }
+        }
+        return totalPairsFullyContained;
+    }
+
+    private static string[] SplitPairs(string line)
+    {
+        return line.Split(',');
+    }
+
+    private static int[] ParseOneRange(string oneRange)
+    {
+        string[] splitInput = oneRange.Split('-');
+        int[] range = { Convert.ToInt32(splitInput[0]), Convert.ToInt32(splitInput[1]) };
+        return range;
     }
     private static int Problem2(string[] lines)
     {
-        return -1;
+        int totalPairsOverlapping = 0;
+        foreach (string line in lines)
+        {
+            string[] pair = SplitPairs(line);
+            int[] firstRange = ParseOneRange(pair[0]);
+            int[] secondRange = ParseOneRange(pair[1]);
+            if (firstRange[0] <= secondRange[0] && firstRange[1] >= secondRange[1])
+            {
+                totalPairsOverlapping++;
+            }
+            else if (secondRange[0] <= firstRange[0] && secondRange[1] >= firstRange[1])
+            {
+                totalPairsOverlapping++;
+            }
+            else if (firstRange[1] >= secondRange[0] && firstRange[1] <= secondRange[1])
+            {
+                totalPairsOverlapping++;
+            }
+            else if (firstRange[0] >= secondRange[0] && firstRange[0] <= secondRange[1])
+            {
+                totalPairsOverlapping++;
+            }
+        }
+        return totalPairsOverlapping;
     }
 }
