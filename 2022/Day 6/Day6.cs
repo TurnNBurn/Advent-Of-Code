9073b3b5792fd18320f4210a031c66352b413264
TurnNBurn <philieaglesfan88@gmail.com>
2022-12-06 08:06:36 +0100

    Solve problems 1 and 2 for day 6

--- 2022/Day 6/Day6.cs
+++ 2022/Day 6/Day6.cs
@@ -1,26 +1,86 @@
 using System;
+using System.Text;
 
 public class AdventOfCode2022Day6
 {
 
     public static void run()
     {
-        string[] lines = System.IO.File.ReadAllLines("./2022/Day 5/Problem1Input.txt");
+        string[] lines = System.IO.File.ReadAllLines("./2022/Day 6/Problem1Input.txt");
 
-        int topCrates = Problem1(lines);
-        //string topCratesCrateMover3001 = Problem2(lines);
+        int firstPacketMarker = Problem1(lines);
+        int firstMessageMarker = Problem2(lines);
 
-        Console.WriteLine("Day 5 - Problem 1: The top crates are " + topCrates + ".");
-        //Console.WriteLine("Day 5 - Problem 2: The top crates are " + topCratesCrateMover3001 + ".");
+        Console.WriteLine("Day 6 - Problem 1: The first packet marker appears after " + firstPacketMarker + " characters.");
+        Console.WriteLine("Day 6 - Problem 2: The first message marker appears after " + firstMessageMarker + " characters.");
     }
 
     private static int Problem1(string[] lines)
     {
+        int i = 0;
+        Dictionary<char, int> buffer = new Dictionary<char, int>();
+        while (i < lines[0].Length)
+        {
+            DictIncrementOrAdd(buffer, lines[0][i]);
+            if (i > 3)
+            {
+                DictRemoveEntry(buffer, lines[0][i - 4]);
+            }
+            if (buffer.Count == 4)
+            {
+                return i + 1;
+            }
+            i++;
+        }
+        //Add one to account for zero index
         return -1;
+    }
+
+    private static void DictRemoveEntry(Dictionary<char, int> dict, char key)
+    {
+        if (dict.ContainsKey(key))
+        {
+            if (dict[key] == 1)
+            {
+                dict.Remove(key);
+            }
+            else
+            {
+                dict[key]--;
+            }
+        }
+    }
+
+    private static void DictIncrementOrAdd(Dictionary<char, int> dict, char key)
+    {
+        if (dict.ContainsKey(key))
+        {
+            dict[key]++;
+        }
+        else
+        {
+            dict.Add(key, 1);
+        }
     }
 
     private static int Problem2(string[] lines)
     {
+        int i = 0;
+        Dictionary<char, int> buffer = new Dictionary<char, int>();
+        while (i < lines[0].Length)
+        {
+            DictIncrementOrAdd(buffer, lines[0][i]);
+            if (i > 13)
+            {
+                DictRemoveEntry(buffer, lines[0][i - 14]);
+            }
+            if (buffer.Count == 14)
+            {
+                return i + 1;
+            }
+            i++;
+        }
+        //Add one to account for zero index
         return -1;
     }
 }
