8fdb06cb6041b9962ddf4c49741498da1375797f
TurnNBurn <philieaglesfan88@gmail.com>
2022-12-21 08:11:35 +0100

    Set up skeletons for Day 18 through Day 21

---
+++ 2022/Day 18/Day18.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text;
+
+public class AdventOfCode2022Day18
+{
+    public static void run()
+    {
+        string[] lines = System.IO.File.ReadAllLines("./2022/Day 18/Problem1Input.txt");
+
+        int totalPressure = Problem1(lines);
+        //long distressFrequency = Problem2(lines);
+
+        Console.WriteLine("Day 18 - Problem 1: The most pressure that can be released is " + totalPressure + ".");
+        //Console.WriteLine("Day 18 - Problem 2: The distress beacon's frequency is " + distressFrequency + ".");
+    }
+
+    private static int Problem1(string[] lines)
+    {
+        return -1;
+    }
+
+    private static int Problem2(string[] lines)
+    {
+        return -1;
+    }
+}
---
+++ 2022/Day 21/Day21.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text;
+
+public class AdventOfCode2022Day21
+{
+    public static void run()
+    {
+        string[] lines = System.IO.File.ReadAllLines("./2022/Day 21/Problem1Input.txt");
+
+        int totalPressure = Problem1(lines);
+        //long distressFrequency = Problem2(lines);
+
+        Console.WriteLine("Day 21 - Problem 1: The most pressure that can be released is " + totalPressure + ".");
+        //Console.WriteLine("Day 21 - Problem 2: The distress beacon's frequency is " + distressFrequency + ".");
+    }
+
+    private static int Problem1(string[] lines)
+    {
+        return -1;
+    }
+
+    private static int Problem2(string[] lines)
+    {
+        return -1;
+    }
+}
