4d234c043bb8d4e8ab521d69f25068985e2e4d26
TurnNBurn <philieaglesfan88@gmail.com>
2023-12-03 22:25:20 +0100

    Solve day 3 problem 2

--- 2023/Day 3/Day3.cs
+++ 2023/Day 3/Day3.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -9,8 +10,8 @@ public class AdventOfCode2023Day3
         string[] lines = System.IO.File.ReadAllLines("./2023/Day 3/Problem1Input.txt");
         int engineSum = Problem1(lines);
         Console.WriteLine("Day 3 - Problem 1: the sum of the engine parts is " + engineSum);
-        //int minPower = Problem2(lines);
-        //Console.WriteLine("Day 3 - Problem 2: the minimum power of the games is " + minPower);
+        int gearRatio = Problem2(lines);
+        Console.WriteLine("Day 3 - Problem 2: the sum of the gear ratios is " + gearRatio);
     }
 
     private static int Problem1(string[] lines)
@@ -21,7 +22,6 @@ public class AdventOfCode2023Day3
         {
             grid[i] = lines[i].ToCharArray();
         }
-        Dictionary<int, int> seenNumbers = new Dictionary<int, int>();
         for (int i = 0; i < lines.Length; i++)
         {
             for (int j = 0; j < lines[i].Length; j++)
@@ -70,5 +70,55 @@ public class AdventOfCode2023Day3
         {
             return GetNum(lines, x, y - 1);
         }
+    }
+
+    private static int Problem2(string[] lines)
+    {
+        int sum = 0;
+        char[][] grid = new char[lines.Length][];
+        for (int i = 0; i < lines.Length; i++)
+        {
+            grid[i] = lines[i].ToCharArray();
+        }
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+
+                if (grid[i][j].Equals('*'))
+                {
+                    sum += IsGear(grid, i, j);
+                }
+            }
+        }
+        return sum;
+    }
+
+    private static int IsGear(char[][] grid, int x, int y)
+    {
+        int numCount = 0;
+        int firstGear = 0;
+        int result = 0;
+        for (int i = -1; i < 2; i++)
+        {
+            for (int j = -1; j < 2; j++)
+            {
+                if (i + x >= 0 && i + x < grid.Length && j + y >= 0 && j + y < grid[i + x].Length)
+                {
+                    if (char.IsNumber(grid[i + x][j + y]))
+                    {
+                        if (numCount > 1)
+                            return 0;
+                        if (firstGear != 0)
+                        {
+                            result = firstGear * GetNum(grid, i + x, j + y);
+                        }
+                        else
+                            firstGear = GetNum(grid, i + x, j + y);
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
