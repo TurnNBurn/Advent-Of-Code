a80f01b34a8a2c8d2f6f6a5a78a0763afc651843
TurnNBurn <philieaglesfan88@gmail.com>
2023-12-03 11:14:30 +0100

    Solve day 2 problem 2

--- 2023/Day 2/Day2.cs
+++ 2023/Day 2/Day2.cs
@@ -5,9 +5,9 @@ public class AdventOfCode2023Day2
     {
         string[] lines = System.IO.File.ReadAllLines("./2023/Day 2/Problem1Input.txt");
         int gameSum = Problem1(lines);
-        Console.WriteLine("Day 1 - Problem 1: the sum of the possible games is " + gameSum);
-        //int realCalibrationSum = Problem2(lines);
-        //Console.WriteLine("Day 1 - Problem 2: the correct sum of the calibration values is " + realCalibrationSum);
+        Console.WriteLine("Day 2 - Problem 1: the sum of the possible games is " + gameSum);
+        int minPower = Problem2(lines);
+        Console.WriteLine("Day 2 - Problem 2: the minimum power of the games is " + minPower);
     }
 
     private static int Problem1(string[] lines)
@@ -56,5 +56,40 @@ public class AdventOfCode2023Day2
             return num < 14;
         else
             return false;
+    }
+
+    private static int Problem2(string[] lines)
+    {
+        int sum = 0;
+        foreach (string line in lines)
+        {
+            string[] pieces = line.Split(':');
+            sum += CalculateOneGame(pieces[1]);
+        }
+        return sum;
+    }
+
+    private static int CalculateOneGame(string game)
+    {
+        int minGreen = int.MinValue;
+        int mindRed = int.MinValue;
+        int minBlue = int.MinValue;
+        string[] rounds = game.Split(';');
+        foreach (string round in rounds)
+        {
+            string[] colors = round.Split(',');
+            foreach (string color in colors)
+            {
+                int num = Convert.ToInt32(color.Trim().Split(" ")[0]);
+                if (color.Contains("green"))
+                    minGreen = Math.Max(minGreen, num);
+                else if (color.Contains("blue"))
+                    minBlue = Math.Max(minBlue, num);
+                else if (color.Contains("red"))
+                    mindRed = Math.Max(mindRed, num);
+            }
+        }
+
+        return minGreen * mindRed * minBlue;
     }
 }
