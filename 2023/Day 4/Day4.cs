36a11e4891745d0eb13cd6fc11b196c651b70d4c
TurnNBurn <Philieaglesfan88@gmail.com>
2023-12-05 09:00:58 +0100

    Update Day4.cs
    Solv Day 4 problem 2

--- 2023/Day 4/Day4.cs
+++ 2023/Day 4/Day4.cs
@@ -6,8 +6,8 @@ public class AdventOfCode2023Day4
         string[] lines = System.IO.File.ReadAllLines("./2023/Day 4/Problem1Input.txt");
         int points = Problem1(lines);
         Console.WriteLine("Day 4 - Problem 1: the sum of the card points is " + points);
-        //int gearRatio = Problem2(lines);
-        //Console.WriteLine("Day 4 - Problem 2: the sum of the gear ratios is " + gearRatio);
+        int totalCards = Problem2(content);
+        Console.WriteLine("Day 2 - The total number of cards is " + totalCards);
     }
 
     private static int Problem1(string[] lines)
@@ -38,6 +38,43 @@ public class AdventOfCode2023Day4
                 }
             }
             sum += cardPoints;
+        }
+        return sum;
+    }
+
+    private static int Problem2(string[] lines)
+    {
+        int[] cardCount = Enumerable.Repeat(1, lines.Length).ToArray();
+        foreach (string line in lines)
+        {
+            HashSet<int> winningNums = new HashSet<int>();
+            string[] pieces = line.Split("|");
+            int gameNum = Convert.ToInt32(pieces[0].Split(":")[0].Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]);
+            string[] winners = pieces[0].Split(":")[1].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            foreach (string winner in winners)
+            {
+                winningNums.Add(Convert.ToInt32(winner.Trim()));
+            }
+
+            int numWinners = 0;
+            foreach (string num in pieces[1].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (winningNums.Contains(Convert.ToInt32(num.Trim())))
+                {
+                    numWinners++;
+                }
+            }
+
+            for (int i = 0; i < numWinners; i++)
+            {
+                cardCount[gameNum + i] += cardCount[gameNum - 1];
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < cardCount.Length; i++)
+        {
+            sum += cardCount[i];
         }
         return sum;
     }
